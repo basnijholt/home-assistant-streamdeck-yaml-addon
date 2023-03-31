e9f5f70d81ce13a0ec22468ee543421ed39ecc48
Bas Nijholt <bas@nijho.lt>
2023-03-31 11:22:14 -0700

    Simplify

--- home-assistant-streamdeck-yaml/rootfs/etc/cont-init.d/home-assistant-streamdeck-yaml.sh
+++ home-assistant-streamdeck-yaml/rootfs/etc/cont-init.d/home-assistant-streamdeck-yaml.sh
@@ -6,20 +6,17 @@
 
 bashio::log.info "Initializing the config overwriting."
 
-CONF=/etc/home-assistant-streamdeck-yaml/.env
-
 hass_host=$(bashio::config "hass_host")
 hass_token=$(bashio::config "hass_token")
 streamdeck_config=$(bashio::config "streamdeck_config")
 websocket_protocol=$(bashio::config "websocket_protocol")
-hass_token_base64=`echo -n $hass_token | base64 -w 0`
 
-sed -i "s/%%HASS_HOST%%/${hass_host}/g" ${CONF}
-sed -i "s/%%HASS_TOKEN%%/${hass_token}/g" ${CONF}
-sed -i "s/%%STREAMDECK_CONFIG%%/${streamdeck_config}/g" ${CONF}
-sed -i "s/%%WEBSOCKET_PROTOCOL%%/${websocket_protocol}/g" ${CONF}
-
-mv ${CONF} /app/.env
+{
+  echo "HASS_HOST=$hass_host"
+  echo "HASS_TOKEN=$hass_token"
+  echo "STREAMDECK_CONFIG=$streamdeck_config"
+  echo "WEBSOCKET_PROTOCOL=$websocket_protocol"
+} > /app/.env
 
 # Copy the Stream Deck YAML configuration file to the add-on
 cp "${streamdeck_config}" /app/configuration.yaml
