c0307fc6598da58296451792f8ba674ae34c6720
Bas Nijholt <bas@nijho.lt>
2023-03-31 13:06:20 -0700

    Robust configuration

--- home-assistant-streamdeck-yaml/rootfs/etc/cont-init.d/home-assistant-streamdeck-yaml.sh
+++ home-assistant-streamdeck-yaml/rootfs/etc/cont-init.d/home-assistant-streamdeck-yaml.sh
@@ -4,7 +4,7 @@
 # Handles configuration
 # ==============================================================================
 
-bashio::log.info "Initializing the config overwriting."
+bashio::log.info "🚀 Initializing the config overwriting."
 
 hass_host=$(bashio::config "hass_host")
 hass_token=$(bashio::config "hass_token")
@@ -13,20 +13,26 @@ websocket_protocol=$(bashio::config "websocket_protocol")
 streamdeck_dotenv=$(bashio::config "streamdeck_dotenv")
 
 if [[ -n "${streamdeck_dotenv}" && -e "${streamdeck_dotenv}" ]]; then
-    bashio::log.info "Using the .env file from ${streamdeck_dotenv}"
+    bashio::log.info "📁 Using the .env file from ${streamdeck_dotenv}"
     # Read STREAMDECK_CONFIG from .env file and copy the Stream Deck YAML configuration file to the add-on
     cp "${streamdeck_dotenv}" /app/.env
     streamdeck_config_from_env=$(grep STREAMDECK_CONFIG "${streamdeck_dotenv}" | cut -d'=' -f2)
 
     # Check if the path is relative
     if [[ "${streamdeck_config_from_env}" != /* ]]; then
-        bashio::log.info "The path to the Stream Deck YAML configuration file is relative, so we assume it is in the same folder as the .env file."
         streamdeck_config_from_env=$(dirname "${streamdeck_dotenv}")/${streamdeck_config_from_env}
+        bashio::log.info "🔍 Detected a relative path for STREAMDECK_CONFIG: ${streamdeck_config_from_env}"
+    else
+        bashio::log.info "🔍 Detected an absolute path for STREAMDECK_CONFIG: ${streamdeck_config_from_env}"
     fi
 
+    bashio::log.info "📂 Copying the Stream Deck YAML configuration file to the add-on: ${streamdeck_config_from_env}"
     cp "${streamdeck_config_from_env}" /app/configuration.yaml
+
+    # Remove STREAMDECK_CONFIG line from /app/.env file
+    sed -i "/^STREAMDECK_CONFIG/d" /app/.env
 else
-    bashio::log.info "Using add-on configuration values instead of .env file"
+    bashio::log.info "🔧 Using add-on configuration values instead of .env file"
     {
         echo "HASS_HOST=$hass_host"
         echo "HASS_TOKEN=$hass_token"
@@ -34,7 +40,8 @@ else
         echo "WEBSOCKET_PROTOCOL=$websocket_protocol"
     } > /app/.env
     # Copy the Stream Deck YAML configuration file to the add-on
+    bashio::log.info "📂 Copying the Stream Deck YAML configuration file to the add-on: ${streamdeck_config}"
     cp "${streamdeck_config}" /app/configuration.yaml
 fi
 
-bashio::log.info "Finished the config overwriting."
+bashio::log.info "✅ Finished the config overwriting."
